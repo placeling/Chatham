2c7c00b09b5228a38f7db280cfa694e8def6708f
Ian MacKinnon <imackinn@alumni.uwaterloo.ca>
2011-07-12 16:37:28 -0700

    Fix mongoid setup and give seed values to users in database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+puts 'EMPTY THE MONGODB DATABASE'
+Mongoid.master.collections.reject { |c| c.name =~ /^system/}.each(&:drop)
+
+puts 'SETTING UP DEFAULT USER LOGIN'
+user = User.create! :username => 'imack', :email => 'imackinn@gmail.com', :password => 'uw2006', :password_confirmation => 'uw2006'
+puts 'New user created: ' << user.username
+user = User.create! :username => '
+lindsayrgwatt', :email => 'lindsayrgwatt@gmail.com ', :password => 'queens2001', :password_confirmation => 'queens2001'
+puts 'New user created: ' << user.username
