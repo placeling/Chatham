1fc745424c07e03970488947ed09457b69fa92c1
Ian MacKinnon <imackinn@alumni.uwaterloo.ca>
2011-07-12 15:53:50 -0700

    Remove standard index page and replace with a home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Chatham::Application.routes.draw do
+  get "home/index"
+
   resources :oauth_clients
 
   match '/oauth/test_request',  :to => 'oauth#test_request',  :as => :test_request
@@ -62,7 +64,7 @@ Chatham::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "home#index"
 
   # See how all your routes lay out with "rake routes"
 
