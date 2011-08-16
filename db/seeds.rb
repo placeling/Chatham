b397c79b292d52c9e9f5ff16865dc19760cbc827
Ian MacKinnon <imackinn@alumni.uwaterloo.ca>
2011-08-16 12:35:08 -0700

    back and fourth :-P

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,16 @@ puts 'EMPTY THE MONGODB DATABASE'
 Mongoid.master.collections.reject { |c| c.name =~ /^system/}.each(&:drop)
 
 puts 'SETTING UP DEFAULT USER LOGIN'
-user = User.create! :admin =>true, :username => 'imack', :email => 'imackinn@gmail.com', :password => 'uw2006', :password_confirmation => 'uw2006'
+user = User.create!(:admin =>true, :username => 'imack',
+                    :email => 'ian@placeling.com', :password => 'uw2006', :password_confirmation => 'uw2006')
+user.admin = true
+user.save
 puts 'New users created: ' << user.username
-user = User.create! :admin => true, :username => 'lindsayrgwatt', :email => 'lindsayrgwatt@gmail.com ', :password => 'queens2001', :password_confirmation => 'queens2001'
+user = User.create!(:admin => true, :username => 'lindsayrgwatt',
+                    :email => 'lindsay@placeling.com ', :password => 'queens2001', :password_confirmation => 'queens2001')
 puts 'New users created: ' << user.username
-
+user.admin = true
+user.save
 
 puts 'creating tyler and his fav spots'
 user = Factory.create(:user, :username=>'tyler')
