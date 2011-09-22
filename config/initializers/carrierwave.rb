ba6e0e341811703a521181019c6f1644f5d69278
Ian MacKinnon <imackinn@alumni.uwaterloo.ca>
2011-09-21 19:08:36 -0700

    add fog for dev, staging, and prod

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,12 +2,27 @@
 
 CarrierWave.configure do |config|
 
-  if Rails.env.production? || Rails.env.staging?
-    # TODO
-    # probably best for amazone s3 to be best here
-    config.storage = :file
-  elsif Rails.env.development? || Rails.env.test?
-    config.storage = :file
+  if Rails.env.test?
+    config.storage = :file #don't want going to s3 for every test suite
+  else
+    config.storage = :fog
+
+    config.fog_public     = false                                   # optional, defaults to true
+    config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+    config.fog_credentials = {
+        :provider               => 'AWS',       # required
+        :aws_access_key_id      => 'AKIAJ5CV27YWN6ERL32Q',       # required
+        :aws_secret_access_key  => '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'       # required
+    }
+
+    if Rails.env.production? || Rails.env.staging?
+      config.fog_directory  = 'chatham-production'                     # required
+    elsif Rails.env.development? || Rails.env.test?
+      #warning, this can be blown away at any moment
+      config.fog_directory  = 'chatham-test'                     # required
+    end
   end
+
+
 
 end
