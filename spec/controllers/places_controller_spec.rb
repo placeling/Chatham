28592866b4f13e04564363b36cddbf160c8610e0
Ian MacKinnon <imackinn@alumni.uwaterloo.ca>
2011-10-02 14:34:42 -0700

    add a test for rendering search page

--- spec/controllers/places_controller_spec.rb
+++ spec/controllers/places_controller_spec.rb
@@ -2,10 +2,25 @@ require 'spec_helper'
 require 'google_places'
 
 describe PlacesController do
+  include Devise::TestHelpers
 
   describe "GET 'places'" do
     it "should be successful" do
       get :index
+      response.should be_success
+    end
+  end
+
+  describe "GET 'search'" do
+    render_views
+    before(:each) do
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      @user = Factory.create(:admin)
+      sign_in @user
+    end
+
+    it "should be successful" do
+      get :search
       response.should be_success
     end
   end
