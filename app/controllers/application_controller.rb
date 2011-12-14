2c46e0dbd9b8f596ed990d8a400c241a4a3bc860
Ian MacKinnon <imackinn@alumni.uwaterloo.ca>
2011-12-13 16:22:44 -0800

    get rid of extraneous login stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,6 @@ class ApplicationController < ActionController::Base
   before_filter :api_check
 
   alias :logged_in? :user_signed_in?
-  before_filter :log_request
-
-
-  def log_request
-    logger.info("Started #{request.method} #{request.url}")
-    for header in request.env.select {|k,v| k.match("^HTTP.*")}
-      logger.info("HEADER #{header}")
-    end
-  end
 
   def after_sign_in_path_for(resource)
     return (session[:"user.return_to"].nil?) ? "/" : session[:"user.return_to"].to_s
