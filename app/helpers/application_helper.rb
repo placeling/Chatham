35e279ddba924054ac521eb03d337093ac8f65fe
Lindsay Watt <lindsayrgwatt@gmail.com>
2012-01-18 18:50:22 -0800

    Turned off debug function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
   # For testing purposes on your localhost. remote_ip always returns 127.0.0.1
-  class ActionController::Request
-    def remote_ip
-      '24.85.231.190'
-    end
-  end
+  #class ActionController::Request
+  #  def remote_ip
+  #    '24.85.231.190'
+  #  end
+  #end
 end
