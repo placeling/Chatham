9781b594d4da8a4d78158d143f3a4ab4351cdd69
Lindsay Watt <lindsayrgwatt@gmail.com>
2012-02-02 16:05:34 -0800

    Fixed 500 error on /search/ GET

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,62 +1,61 @@
 require 'google_places_autocomplete'
 
 class SearchController < ApplicationController
-  before_filter :parameters_required, :only=>[:search]
-  
-  def parameters_required
-    if params[:lat].nil? or params[:lng].nil? or params[:input].nil?
-      redirect_to root_path
-    end
-    
-    if params[:lat].to_f == 0.0 and params[:lat] != "0.0"
-      redirect_to root_path
-    end
-    
-    if params[:lng].to_f == 0.0 and params[:lng] != "0.0"
-      redirect_to root_path
-    end
-    
-  end
-  
   def search
     @results = []
     
-    # places
-    lat = params[:lat].to_f
-    lng = params[:lng].to_f
-    gpa = GooglePlacesAutocomplete.new
+    @valid_params = true
     
-    @input = params[:input]
-    
-    raw_places = gpa.suggest(lat, lng, 10000, @input)
-    
-    if !raw_places.nil?
-      raw_places.each do |place|
-        interstitial = {}
-        interstitial['name'] = place.terms[0].value
-        interstitial['url'] = reference_places_path + "?ref=" + place.reference
-        location = []
-        place.terms.each_with_index do |term, index|
-          if index != 0
-            location << term.value
-          end
-        end
-        interstitial['location'] = location.join(", ")
-        @results << interstitial
-      end
+    if params[:lat].nil? or params[:lng].nil? or params[:input].nil?
+      @valid_params = false
     end
     
-    # people
-    raw_users = User.search_by_username(@input)
+    if params[:lat].to_f == 0.0 and params[:lat] != "0.0"
+      @valid_params = false
+    end
     
-    if !raw_users.nil?
-      raw_users.each do |user|
-        interstitial = {}
-        interstitial['name'] = user.username
-        interstitial['location'] = user.city
-        interstitial['url'] = user_path(user)
-        interstitial['pic'] = user.thumb_url
-        @results << interstitial
+    if params[:lng].to_f == 0.0 and params[:lng] != "0.0"
+      @valid_params = false
+    end
+    
+    if @valid_params
+      # places
+      lat = params[:lat].to_f
+      lng = params[:lng].to_f
+      gpa = GooglePlacesAutocomplete.new
+
+      @input = params[:input].force_encoding("UTF-8")
+
+      raw_places = gpa.suggest(lat, lng, 10000, @input)
+
+      if !raw_places.nil?
+        raw_places.each do |place|
+          interstitial = {}
+          interstitial['name'] = place.terms[0].value
+          interstitial['url'] = reference_places_path + "?ref=" + place.reference
+          location = []
+          place.terms.each_with_index do |term, index|
+            if index != 0
+              location << term.value
+            end
+          end
+          interstitial['location'] = location.join(", ")
+          @results << interstitial
+        end
+      end
+
+      # people
+      raw_users = User.search_by_username(@input)
+
+      if !raw_users.nil?
+        raw_users.each do |user|
+          interstitial = {}
+          interstitial['name'] = user.username
+          interstitial['location'] = user.city
+          interstitial['url'] = user_path(user)
+          interstitial['pic'] = user.thumb_url
+          @results << interstitial
+        end
       end
     end
     
