b5e63f1490a3be4e7ddc8ba3239ca9a64f32172b
Ian MacKinnon <imackinn@alumni.uwaterloo.ca>
2012-02-22 20:27:23 -0800

    SSL problems, hopefully this will get the thing working again

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, CHATHAM_CONFIG['facebook_app_id'], CHATHAM_CONFIG['facebook_app_secret']
+  provider :facebook, CHATHAM_CONFIG['facebook_app_id'], CHATHAM_CONFIG['facebook_app_secret'], {:client_options => {:ssl => {:ca_file => "/usr/lib/ssl/certs/ca-certificates.crt"}}}
   #provider :gowalla, CHATHAM_CONFIG['gowalla_api_key'], CHATHAM_CONFIG['gowalla_secret_key']
 end
