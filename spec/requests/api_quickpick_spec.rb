0c79e253dda55563e917eb15439fd27e176b8977
Ian MacKinnon <imackinn@alumni.uwaterloo.ca>
2012-03-31 11:26:51 -0700

    increase number of nearby places in the quick pick test to 5 so it doesn't grab "barrie" results

--- spec/requests/api_quickpick_spec.rb
+++ spec/requests/api_quickpick_spec.rb
@@ -46,11 +46,16 @@ describe "Quick Picks" do
 
     place = Factory.create(:lib_square)
     place2 = Factory.create(:new_place)
+
     place3 = Factory.create(:lib_square, :name => "secondary place")
+    place4 = Factory.create(:lib_square, :name => "ternary place")
+    place5 = Factory.create(:lib_square, :name => "quadranary place")
 
     perspective = Factory.create(:perspective, :memo=>"one", :place =>place, :user =>user2)
     perspective2 = Factory.create(:perspective, :memo=>"two", :place =>place2, :user =>user3)
     perspective3 = Factory.create(:perspective, :memo=>"three", :place =>place3, :user =>user4)
+    perspective4 = Factory.create(:perspective, :memo=>"three", :place =>place4, :user =>user2)
+    perspective5 = Factory.create(:perspective, :memo=>"three", :place =>place5, :user =>user3)
 
     post_via_redirect user_session_path, 'user[login]' => user.username, 'user[password]' => user.password
 
@@ -58,13 +63,13 @@ describe "Quick Picks" do
       :format => 'json',
       :lat => '49.282049',
       :lng => '-123.107772',
-      :socialgraph =>false
+      :query_type =>'popular'
     }
 
     response.status.should be(200)
 
     showPlace = JSON.parse( response.body )
-    showPlace['suggested_places'].count.should == 3
+    showPlace['suggested_places'].count.should == 5
 
   end
 
