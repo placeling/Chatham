81893e1f798bef7add9c8fb9e8ab15d08a3d1f4b
Ian MacKinnon <imackinn@alumni.uwaterloo.ca>
2012-04-11 16:05:14 -0700

    more to a more general redis model rather than a resque specific one. keep a redis connect global for app

--- config/initializers/redis_resque.rb
+++ config/initializers/redis_resque.rb
@@ -1,4 +1,8 @@
 
-resque_config = YAML.load_file("#{::Rails.root.to_s}/config/resque.yml")
-Resque.redis = resque_config[::Rails.env]
+REDIS_CONFIG = YAML.load( File.open( Rails.root.join("config/redis.yml") ) )
+
+$redis = Redis.new(REDIS_CONFIG)
+$redis.flushdb if Rails.env.test?
+
+Resque.redis = $redis
 Resque.redis.namespace = "resque:placeling"
