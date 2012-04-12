5d94c3bb2fd0947ffe5c1023a1a529dbb4ffe475
Ian MacKinnon <imackinn@alumni.uwaterloo.ca>
2012-04-11 23:28:19 -0700

    user namespace for app

--- config/initializers/redis_resque.rb
+++ config/initializers/redis_resque.rb
@@ -1,8 +1,10 @@
 
 REDIS_CONFIG = YAML.load( File.open( Rails.root.join("config/redis.yml") ) )
 
-$redis = Redis.new(REDIS_CONFIG[::Rails.env].symbolize_keys!)
-$redis.flushdb if Rails.env.test?
+redis_base = Redis.new(REDIS_CONFIG[::Rails.env].symbolize_keys!)
 
-Resque.redis = $redis
+Resque.redis = redis_base
 Resque.redis.namespace = "resque:placeling"
+
+$redis = Redis::Namespace.new(REDIS_CONFIG[::Rails.env][:namespace], :redis => redis_base)
+$redis.flushdb if Rails.env.test?
