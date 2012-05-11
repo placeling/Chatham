d461606354866cd6383708700352f6cca0281b80
Lindsay Watt <lindsayrgwatt@gmail.com>
2012-05-10 20:58:47 -0700

    Updated migration to properly set iPhone status

--- db/migrate/20120509214901_add_first_run_settings.rb
+++ db/migrate/20120509214901_add_first_run_settings.rb
@@ -1,5 +1,8 @@
 class AddFirstRunSettings < Mongoid::Migration
   def self.up
+    
+    ca = ClientApplication.find("4e73c371a6f1ca1278000003") #nina
+    
     User.all.each do |user|
       puts "Attaching first run info to #{user.username}"
       user.attach_first_run
@@ -8,8 +11,13 @@ class AddFirstRunSettings < Mongoid::Migration
       user.first_run.placemark = false
       user.first_run.map = false
       
-      user.first_run.dismiss_app_ad = false
-      user.first_run.downloaded_app = false
+      if ca.tokens.where(:uid => user.id).entries.count == 0
+        user.first_run.dismiss_app_ad = false
+        user.first_run.downloaded_app = false
+      else
+        user.first_run.dismiss_app_ad = true
+        user.first_run.downloaded_app = true
+      end
       
       user.save
     end
