465e52f20a81160a3a8626a5831859e7d435f6f6
Ian MacKinnon <imackinn@alumni.uwaterloo.ca>
2012-05-18 18:57:09 -0700

    more harmony of prod/staging asset issues

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -29,10 +29,6 @@ Chatham::Application.configure do
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
 
-  # Disable Rails's static asset server
-  # In production, Apache or nginx will already do this
-  config.serve_static_assets = false
-
   config.force_ssl = true
 
   config.assets.compress = true
