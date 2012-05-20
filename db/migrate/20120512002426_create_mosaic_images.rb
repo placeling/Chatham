20eec5c5a1301abb4b0f60dc1574d41dd14a4db8
Ian MacKinnon <imackinn@alumni.uwaterloo.ca>
2012-05-20 15:12:37 -0700

    change how iteration over perspective happens in hope of garbage collector grabbing closed files

--- db/migrate/20120512002426_create_mosaic_images.rb
+++ db/migrate/20120512002426_create_mosaic_images.rb
@@ -1,9 +1,15 @@
 class CreateMosaicImages < Mongoid::Migration
   def self.up
-    Perspective.all.each do |perp|
+    perspectives = Perspective.all.excludes(:pictures => nil).to_a
+    puts "Need to process #{perspectives.count} files"
+
+    int i=0
+    while perp = perspectives.pop
+      i = i+1
       perp.pictures.each do |picture|
         begin
           if picture.creation_environment == Rails.env && picture.mosaic_3_2_cache_url.nil?
+            puts "Processing Perspective #{i}"
             picture.image.cache_stored_file!
             picture.image.retrieve_from_cache!(picture.image.cache_name)
             
@@ -11,7 +17,7 @@ class CreateMosaicImages < Mongoid::Migration
             
             picture.image.recreate_versions!
             picture.save!
-            CarrierWave.clean_cached_files!
+            picture.file.delete #get rid cached version
           end
         rescue => e
           puts  "ERROR: perspective #{perp.id}: picture #{picture.id} -> #{e.to_s}"
