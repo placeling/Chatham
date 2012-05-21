d3572f37ff415586315070de545ea318f7dfe0a5
Ian MacKinnon <imackinn@alumni.uwaterloo.ca>
2012-05-21 15:05:20 -0700

    background location check for notification

---
+++ app/workers/check_location_highlights.rb
@@ -0,0 +1,23 @@
+class CheckLocationHighlights
+  @queue = :apns_queue
+
+  def self.perform(user_id, lat, lng, accuracy, timestamp)
+
+    if timestamp + 60 < Time.now.to_i || accuracy >500
+      #would be out of date, probably not valid
+      return
+    end
+
+    user = User.find( user_id )
+    perspectives = Perspective.find_nearby_for_user( user, [lat,lng], 0.004, 0, 20 )
+
+    perspectives.each do |perspective|
+      if user.highlighted_places.include? perspective.place.id
+        if user.ios_notification_token
+          Resque.enqueue(SendNotifications, user.id, "#{perspective.place.name} is near you!", "placeling://places/#{perspective.place.id}")
+        end
+        break
+      end
+    end
+  end
+end
