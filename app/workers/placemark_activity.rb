be8dd63b65c9b5a4b08573f05974b7463d4b77d1
Ian MacKinnon <imackinn@alumni.uwaterloo.ca>
2012-05-25 19:08:47 -0700

    send photo for new placemark, but dont' send to facebook for non-updating placemark

--- app/workers/placemark_activity.rb
+++ app/workers/placemark_activity.rb
@@ -16,8 +16,25 @@ class PlacemarkActivity
     activity.save
     activity.push_to_followers( actor1 )
 
-    if fb_post && actor1.facebook && !(Rails.env.development? || Rails.env.test?)#use in all environments
-      actor1.facebook.og_action!("placeling:placemark", :location => perspective.og_path )
+    if fb_post && actor1.facebook && !(Rails.env.development? || Rails.env.test?)
+      image_url=nil
+      for picture in perspective.pictures
+        if !picture.fb_posted
+          image_url = picture.main_cache_url
+          picture.fb_posted = true
+          picture.save
+          break
+        end
+      end
+
+      if !image_url.nil?
+        actor1.facebook.og_action!("placeling:placemark",
+                                 :location => perspective.og_path,
+                                 "image[0][url]" => perspective.pictures[0].main_cache_url,
+                                  "image[0][user_generated]" =>true)
+      else
+        actor1.facebook.og_action!("placeling:placemark",:location => perspective.og_path)
+      end
     end
   end
 end
