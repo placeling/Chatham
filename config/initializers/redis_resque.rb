7b2599faf4dd61ec855f863b2e2d0028cffca422
Ian MacKinnon <imackinn@alumni.uwaterloo.ca>
2012-05-29 10:24:33 -0700

    add airbrake to resque failure handling

--- config/initializers/redis_resque.rb
+++ config/initializers/redis_resque.rb
@@ -5,7 +5,16 @@ redis_base = Redis.new(REDIS_CONFIG[::Rails.env].symbolize_keys!)
 
 Resque.redis = redis_base
 Resque.redis.namespace = "resque:placeling"
+
+Resque::Failure::Airbrake.configure do |config|
+  config.api_key = '6cdd0c13c32d9b5cbd3ab510033319af'
+end
+
+Resque::Failure::Multiple.classes = [Resque::Failure::Redis, Resque::Failure::Airbrake]
+Resque::Failure.backend = Resque::Failure::Multiple
+
 #Resque.schedule = YAML.load_file(File.join(Rails.root, 'config/resque_schedule.yml'))
 
 $redis = Redis::Namespace.new(REDIS_CONFIG[::Rails.env][:namespace], :redis => redis_base)
 $redis.flushdb if Rails.env.test?
+
