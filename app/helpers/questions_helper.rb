bb6cb30ff881265b41e1da4ae10c936c99a1470f
Ian MacKinnon <imackinn@alumni.uwaterloo.ca>
2012-06-14 12:16:04 -0700

    center map at locus of top 5 points

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -20,10 +20,25 @@ module QuestionsHelper
   end
 
   def map_url( question )
-    url = "https://maps.googleapis.com/maps/api/staticmap?center=#{ question.location[0]},#{ question.location[1] }&zoom=11&size=258x198&sensor=false"
+    url = "https://maps.googleapis.com/maps/api/staticmap?zoom=12&size=258x198&sensor=false"
 
-    question.answers.each do |answer|
-      url += "&markers=icon:http://www.placeling.com/images/marker.png%7Ccolor:red%7C#{answer.place.location[0]},#{answer.place.location[1]}" unless answer.new_record?
+    tally = 0
+    lat = 0.0
+    lng = 0.0
+    question.answers.order_by([[:upvotes, :desc]]).limit(5).each do |answer|
+      unless answer.new_record?
+        url += "&markers=icon:http://www.placeling.com/images/marker.png%7Ccolor:red%7C#{answer.place.location[0]},#{answer.place.location[1]}"
+        tally += 1
+        lat += answer.place.location[0]
+        lng += answer.place.location[1]
+      end
+
+    end
+
+    if tally > 1
+      url += "&center=#{ lat/tally },#{ lng/tally }"
+    else
+      url += "&center=#{ question.location[0]},#{ question.location[1] }"
     end
     return url
   end
