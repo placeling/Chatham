59d29a94a2f0b59715208a3efc5b1497c7255ff4
Ian MacKinnon <imackinn@alumni.uwaterloo.ca>
2012-06-14 12:01:37 -0700

    put markers on map

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -19,4 +19,13 @@ module QuestionsHelper
 
   end
 
+  def map_url( question )
+    url = "https://maps.googleapis.com/maps/api/staticmap?center=#{ question.location[0]},#{ question.location[1] }&zoom=11&size=258x198&sensor=false"
+
+    question.answers.each do |answer|
+      url += "&markers=icon:http://www.placeling.com/images/marker.png%7Ccolor:red%7C#{answer.place.location[0]},#{answer.place.location[1]}" unless answer.new_record?
+    end
+    return url
+  end
+
 end
