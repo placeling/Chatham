d6de8fcb72b9ce1a359662c3154eb63ecc5c438b
Ian MacKinnon <imackinn@alumni.uwaterloo.ca>
2012-06-19 12:32:15 -0700

    fixup mixpanel stuff

--- config/initializers/mixpanel.rb
+++ config/initializers/mixpanel.rb
@@ -1,4 +1,4 @@
-if Rails.env.production?
+if true #Rails.env.production?
   MIXPANEL_TOKEN = "de55e84dbfc779ff7e9174189255fe2c"
   Chatham::Application.config.middleware.use "Mixpanel::Tracker::Middleware", MIXPANEL_TOKEN
 else
