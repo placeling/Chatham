3d2a52e2fdea5563f864a4c8c1ede11f615f352a
Ian MacKinnon <imackinn@alumni.uwaterloo.ca>
2012-06-19 11:34:55 -0700

    set my application

--- config/initializers/mixpanel.rb
+++ config/initializers/mixpanel.rb
@@ -1,6 +1,6 @@
 if Rails.env.production?
   MIXPANEL_TOKEN = "de55e84dbfc779ff7e9174189255fe2c"
-  YourApplication::Application.config.middleware.use "Mixpanel::Tracker::Middleware", MIXPANEL_TOKEN
+  Chatham::Application.config.middleware.use "Mixpanel::Tracker::Middleware", MIXPANEL_TOKEN
 else
   class DummyMixpanel
     def method_missing(m, *args, &block)
