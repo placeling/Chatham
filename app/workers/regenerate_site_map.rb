7a0dace168a7462e34426b057718e337b27daf54
imackinn <imackinn@alumni.uwaterloo.ca>
2012-06-21 16:50:58 -0700

    add resque scheduler hooks for regerating site map automagically

---
+++ app/workers/regenerate_site_map.rb
@@ -0,0 +1,13 @@
+require 'sitemap_generator/tasks'
+
+class RegenerateSiteMap
+  @queue = :admin
+  def self.perform()
+    if Rails.env.production?
+      SitemapGenerator::Utilities.clean_files
+      SitemapGenerator::Interpreter.run(:config_file => ENV["CONFIG_FILE"], :verbose => verbose)
+      SitemapGenerator::Sitemap.ping_search_engines
+    end
+
+  end
+end
--- config/initializers/redis_resque.rb
+++ config/initializers/redis_resque.rb
@@ -22,7 +22,7 @@ unless defined?(RESQUE_LOGGER)
   RESQUE_LOGGER = ActiveSupport::BufferedLogger.new f
 end
 
-#Resque.schedule = YAML.load_file(File.join(Rails.root, 'config/resque_schedule.yml'))
+Resque.schedule = YAML.load_file(File.join(Rails.root, 'config/resque_schedule.yml'))
 
 $redis = Redis::Namespace.new(REDIS_CONFIG[::Rails.env][:namespace], :redis => redis_base)
 $redis.flushdb if Rails.env.test?
