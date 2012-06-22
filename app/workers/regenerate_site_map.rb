7dbe973d0b0eb4b657c87e8f16abc7e6a887e14e
imackinn <imackinn@alumni.uwaterloo.ca>
2012-06-22 09:54:04 -0700

    put a value in for verbose

--- app/workers/regenerate_site_map.rb
+++ app/workers/regenerate_site_map.rb
@@ -4,9 +4,9 @@ class RegenerateSiteMap
   @queue = :admin
   def self.perform()
     if Rails.env.production?
-      SitemapGenerator::Utilities.clean_files
-      SitemapGenerator::Interpreter.run(:config_file => ENV["CONFIG_FILE"], :verbose => verbose)
-      SitemapGenerator::Sitemap.ping_search_engines
+    SitemapGenerator::Utilities.clean_files
+    SitemapGenerator::Interpreter.run(:config_file => ENV["CONFIG_FILE"], :verbose => false)
+    SitemapGenerator::Sitemap.ping_search_engines
     end
 
   end
