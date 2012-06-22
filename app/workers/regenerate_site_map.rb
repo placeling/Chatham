d77add73c6337852809fcbe910a88afb26889274
imackinn <imackinn@alumni.uwaterloo.ca>
2012-06-22 12:26:22 -0700

    fixup sitemap stuffs

--- app/workers/regenerate_site_map.rb
+++ app/workers/regenerate_site_map.rb
@@ -3,11 +3,12 @@ require 'sitemap_generator/tasks'
 class RegenerateSiteMap
   @queue = :admin
   def self.perform()
-    if Rails.env.production?
+    #if Rails.env.production?
+    SitemapGenerator::Sitemap.sitemaps_path = 'shared/'
     SitemapGenerator::Utilities.clean_files
     SitemapGenerator::Interpreter.run(:config_file => ENV["CONFIG_FILE"], :verbose => false)
     SitemapGenerator::Sitemap.ping_search_engines
-    end
+    #end
 
   end
 end
