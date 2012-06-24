ad821db92128f099a0c5df5109bf3cb242aa7316
imackinn <imackinn@alumni.uwaterloo.ca>
2012-06-23 19:16:43 -0700

    more publish actions

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, CHATHAM_CONFIG['facebook_app_id'], CHATHAM_CONFIG['facebook_app_secret'], {:scope => "email, publish_stream,offline_access", :client_options => {:ssl => {:ca_file => "/usr/lib/ssl/certs/ca-certificates.crt"}}}
+  provider :facebook, CHATHAM_CONFIG['facebook_app_id'], CHATHAM_CONFIG['facebook_app_secret'], {:scope => "email, publish_stream, publish_actions,offline_access", :client_options => {:ssl => {:ca_file => "/usr/lib/ssl/certs/ca-certificates.crt"}}}
   #provider :gowalla, CHATHAM_CONFIG['gowalla_api_key'], CHATHAM_CONFIG['gowalla_secret_key']
 end
