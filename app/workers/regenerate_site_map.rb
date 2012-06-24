ddb5c08e4d4123de4348ac9085eda07c1c433c12
imackinn <imackinn@alumni.uwaterloo.ca>
2012-06-24 11:08:37 -0700

    fixup the site mapper to generate to shared directory, and update robots.txt to reflect new url

--- app/workers/regenerate_site_map.rb
+++ app/workers/regenerate_site_map.rb
@@ -4,7 +4,7 @@ class RegenerateSiteMap
   @queue = :admin
   def self.perform()
     #if Rails.env.production?
-    SitemapGenerator::Sitemap.sitemaps_path = 'shared/'
+    SitemapGenerator::Sitemap.sitemaps_path = 'system/'
     SitemapGenerator::Utilities.clean_files
     SitemapGenerator::Interpreter.run(:config_file => ENV["CONFIG_FILE"], :verbose => false)
     SitemapGenerator::Sitemap.ping_search_engines
