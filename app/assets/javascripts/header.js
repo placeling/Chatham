a42668e2aff6e0a5a5d1af8f1bcc1cc1e24a2b54
imackinn <imackinn@alumni.uwaterloo.ca>
2012-06-23 22:30:44 -0700

    fixup the UTF-8 stuff having to do with the autocomplete

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,66 +1,66 @@
-$(document).ready(function(){
-  var userLocation = JSON.parse($.cookie('location'));
-  
-  var user_lat = 49.2;
-  var user_lng = -123.2;
-  
-  if ((typeof userLocation !== "undefined") && (userLocation !== null)) {
-    if (typeof userLocation["remote_ip"] !== "undefined") {
-      user_lat = parseFloat(userLocation["remote_ip"]["lat"]);
-      user_lng = parseFloat(userLocation["remote_ip"]["lng"]);
-    } else if (typeof userLocation["user"] !== "undefined") {
-      user_lat = parseFloat(userLocation["user"]["lat"]);
-      user_lng = parseFloat(userLocation["user"]["lng"]);
-    } else {
-      user_lat = parseFloat(userLocation["default"]["lat"]);
-      user_lng = parseFloat(userLocation["default"]["lng"]);
+$(document).ready(function () {
+    var userLocation = JSON.parse($.cookie('location'));
+
+    var user_lat = 49.2;
+    var user_lng = -123.2;
+
+    if ((typeof userLocation !== "undefined") && (userLocation !== null)) {
+        if (typeof userLocation["remote_ip"] !== "undefined") {
+            user_lat = parseFloat(userLocation["remote_ip"]["lat"]);
+            user_lng = parseFloat(userLocation["remote_ip"]["lng"]);
+        } else if (typeof userLocation["user"] !== "undefined") {
+            user_lat = parseFloat(userLocation["user"]["lat"]);
+            user_lng = parseFloat(userLocation["user"]["lng"]);
+        } else {
+            user_lat = parseFloat(userLocation["default"]["lat"]);
+            user_lng = parseFloat(userLocation["default"]["lng"]);
+        }
     }
-  }
 
 
-  $("#term").autocomplete({
-    minLength: 3,
-    html: true,
-    select: function(e, ui) {
-      var findText = '<div class="hidden" id="url">';
-      var url = ui.item.value.substring(ui.item.value.indexOf(findText)+findText.length, ui.item.value.length-6);
-      window.location = url;
-      return false;
-    },
-    focus: function(e, ui) {
-      var startText = '<div class="autocomplete_name">'
-      var endText = '</div><div class="autocomplete_location">'
-      var name = ui.item.value.substring(ui.item.value.indexOf(startText)+startText.length, ui.item.value.indexOf(endText));
-      $("#term").val(name);
-      return false;
-    },
-    source: function(req, add) {
-      var url = "/search/?lat="+user_lat+"&lng="+user_lng+"&input="+escape(req["term"]);
-      
-      $.getJSON(url, function(data) {
-        var results = data.results;
-        var suggestions = [];
-        
-        $.each(results, function(i, val) {
-          var text = "";
-          if (val["pic"]) {
-            text = '<div class="autocomplete_thumb"><img class="autocomplete_thumb_img" src="'+val["pic"]+'"/></div><div class="autocomplete_thumb_holder"><div class="autocomplete_name">'+val["name"]+'</div><div class="autocomplete_location">'+val["location"]+'</div></div><div class="hidden" id="url">'+val["url"]+'</div>';
-          } else {
-            text = '<div class="autocomplete_holder"><div class="autocomplete_name">'+val["name"]+'</div><div class="autocomplete_location">'+val["location"]+'</div></div><div class="hidden" id="url">'+val["url"]+'</div>';
-          }
-          suggestions.push(text);
-        });
-        
-        add(suggestions);
-      });
-    }
-  });
-  
-  $("#search_submit").click(function(){
-    if ($("#term").val() != "") {
-      var url = "/search/?lat="+user_lat+"&lng="+user_lng+"&input="+escape($("#term").val());
-      window.location = url;
-      return false;
-    }
-  });
+    $("#term").autocomplete({
+        minLength:3,
+        html:true,
+        select:function (e, ui) {
+            var findText = '<div class="hidden" id="url">';
+            var url = ui.item.value.substring(ui.item.value.indexOf(findText) + findText.length, ui.item.value.length - 6);
+            window.location = url;
+            return false;
+        },
+        focus:function (e, ui) {
+            var startText = '<div class="autocomplete_name">'
+            var endText = '</div><div class="autocomplete_location">'
+            var name = ui.item.value.substring(ui.item.value.indexOf(startText) + startText.length, ui.item.value.indexOf(endText));
+            $("#term").val(name);
+            return false;
+        },
+        source:function (req, add) {
+            var url = "/search/?lat=" + user_lat + "&lng=" + user_lng + "&input=" + encodeURIComponent(req["term"]);
+
+            $.getJSON(url, function (data) {
+                var results = data.results;
+                var suggestions = [];
+
+                $.each(results, function (i, val) {
+                    var text = "";
+                    if (val["pic"]) {
+                        text = '<div class="autocomplete_thumb"><img class="autocomplete_thumb_img" src="' + val["pic"] + '"/></div><div class="autocomplete_thumb_holder"><div class="autocomplete_name">' + val["name"] + '</div><div class="autocomplete_location">' + val["location"] + '</div></div><div class="hidden" id="url">' + val["url"] + '</div>';
+                    } else {
+                        text = '<div class="autocomplete_holder"><div class="autocomplete_name">' + val["name"] + '</div><div class="autocomplete_location">' + val["location"] + '</div></div><div class="hidden" id="url">' + val["url"] + '</div>';
+                    }
+                    suggestions.push(text);
+                });
+
+                add(suggestions);
+            });
+        }
+    });
+
+    $("#search_submit").click(function () {
+        if ($("#term").val() != "") {
+            var url = "/search/?lat=" + user_lat + "&lng=" + user_lng + "&input=" + escape($("#term").val());
+            window.location = url;
+            return false;
+        }
+    });
 });
--- lib/google_places_autocomplete.rb
+++ lib/google_places_autocomplete.rb
@@ -3,67 +3,67 @@ require 'json'
 class GooglePlacesAutocomplete
   include HTTParty
   debug_output $stdout
-  
+
   # api docs: http://code.google.com/apis/maps/documentation/places/autocomplete.html
   base_uri "https://maps.googleapis.com/maps/api/place/autocomplete"
-  
+
   def initialize()
     @api_key = CHATHAM_CONFIG['google_api']
   end
-  
-  
+
+
   # NOTE: need to keep following synced with what we do in the mobile client
   def suggest(x, y, input, sensor = true, language ="en")
-    location = [x.round(4),y.round(4)].join(',')
-    
+    location = [x.round(4), y.round(4)].join(',')
+
     options = {
-      :location => location,
-      :sensor => sensor,
-      :input => input,
+        :location => location,
+        :sensor => sensor,
+        :input => input
     }
 
-    results = mashup( self.class.get("/json", :query => options.merge(self.default_options)) ).predictions
-    
+    results = mashup(self.class.get("/json", :query => options.merge(self.default_options))).predictions
+
     return results
   end
-  
+
   #for testing
   def api_key
     return @api_key
   end
-  
+
   def api_key=(value)
     @api_key = value
   end
-  
+
   protected
-    def default_options
-      { :key => @api_key }
-    end
-    
-    def mashup(response)
-      if response.code == 200
-        if response.is_a?(Hash)
-          hash = Hashie::Mash.new(response)
-        else
-          if response.first.is_a?(Hash)
-            hash = response.map{|item| Hashie::Mash.new(item)}
-          else
-            response
-          end
-        end
-        
-        if hash.status == "OK" or hash.status == "ZERO_RESULTS"
-          return hash
-        elsif hash.status == "REQUEST_DENIED"
-          raise "Bad Google Places Request - request denied"
-        elsif hash.status == "OVER_QUERY_LIMIT"
-          raise "Bad Google Places Request - OVER QUERY LIMIT"
-        elsif hash.status == "INVALID_REQUEST"
-          raise "Bad Google Places Request - INVALID REQUEST"
-        end
+  def default_options
+    {:key => @api_key}
+  end
+
+  def mashup(response)
+    if response.code == 200
+      if response.is_a?(Hash)
+        hash = Hashie::Mash.new(response)
       else
-        raise "Google Places API returned non-200 result"
+        if response.first.is_a?(Hash)
+          hash = response.map { |item| Hashie::Mash.new(item) }
+        else
+          response
+        end
       end
+
+      if hash.status == "OK" or hash.status == "ZERO_RESULTS"
+        return hash
+      elsif hash.status == "REQUEST_DENIED"
+        raise "Bad Google Places Request - request denied"
+      elsif hash.status == "OVER_QUERY_LIMIT"
+        raise "Bad Google Places Request - OVER QUERY LIMIT"
+      elsif hash.status == "INVALID_REQUEST"
+        raise "Bad Google Places Request - INVALID REQUEST"
+      end
+    else
+      raise "Google Places API returned non-200 result"
     end
+  end
 end
