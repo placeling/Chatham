ef77647446660068b83f03bd7c2659edde62c7ad
imackinn <imackinn@alumni.uwaterloo.ca>
2012-06-27 21:25:25 -0700

    CHA-236, add autocomplete results to the search results

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,29 +3,29 @@ require 'google_reverse_geocode'
 
 class PlacesController < ApplicationController
   before_filter :login_required, :only => [:new, :confirm, :create, :new, :update, :destroy, :highlight, :unhighlight]
-  
+
   def reference
     if params[:ref].nil?
       raise ActionController::RoutingError.new('Not Found')
     end
-    
+
     gp = GooglePlaces.new
     place = gp.get_place(params[:ref])
-    
+
     if place.nil?
       raise ActionController::RoutingError.new('Not Found')
     end
-    
-    @place = Place.find_by_google_id( place.id )
-    
+
+    @place = Place.find_by_google_id(place.id)
+
     if @place.nil?
-      @place = Place.new_from_google_place( place )
+      @place = Place.new_from_google_place(place)
       @place.save
     end
-    
+
     redirect_to place_path(@place)
   end
-  
+
   def nearby
     lat = params[:lat].to_f
     lng = params[:lng].to_f
@@ -46,7 +46,7 @@ class PlacesController < ApplicationController
 
     for place in @places
       #add distance to in meters
-      place.distance = (1000 * Geocoder::Calculations.distance_between([lat,lng], [place.geometry.location.lat,place.geometry.location.lng], :units =>:km)).floor
+      place.distance = (1000 * Geocoder::Calculations.distance_between([lat, lng], [place.geometry.location.lat, place.geometry.location.lng], :units => :km)).floor
     end
 
     #TEST: sort by distance rather than popularity
@@ -54,7 +54,7 @@ class PlacesController < ApplicationController
 
     respond_to do |format|
       format.html
-      format.json { render :json => {:places => @places } }
+      format.json { render :json => {:places => @places} }
     end
 
   end
@@ -68,52 +68,52 @@ class PlacesController < ApplicationController
     #@places = @places.sort_by { |place| place.distance }
 
     respond_to do |format|
-      format.html { render :show}
+      format.html { render :show }
       format.json { render :json => @place }
     end
 
   end
-  
+
   def search
     @user = current_user
 
     lat = params[:lat].to_f
     lng = params[:lng].to_f
 
-    if ( params[:long] )
+    if (params[:long])
       lng = params[:long].to_f
     end
-    
+
     if lat && lng && params[:query] && params[:query].length > 0
       radius = 500
       gp = GooglePlaces.new
-      
+
       query = params[:query]
       if params[:query].length > 0
         @query = params[:query]
       end
-      
+
       @places = []
-      
+
       # Google Places
       @raw_places = gp.find_nearby(lat, lng, radius, query)
       if @raw_places.length > 0
         @raw_places.each do |place|
-          @place = Place.find_by_google_id( place.id )
-          
+          @place = Place.find_by_google_id(place.id)
+
           if @place.nil?
             #not here, and we need to fetch it
             gp = GooglePlaces.new
-            @place = Place.new_from_google_place( gp.get_place( place.reference ) )
+            @place = Place.new_from_google_place(gp.get_place(place.reference))
             @place.user = current_user unless current_user.nil?
             @place.client_application = current_client_application unless current_client_application.nil?
             @place.save!
           end
-          
+
           @places.push(@place)
         end
       end
-      
+
       # Our database - need to do as Google doesn't always find places we add (e.g., Siwash Rock)
       # NOTE: This is hacky as do not have case-insensitive search in MongoDB
       query_term = params[:query].strip.split(",")[0]
@@ -126,12 +126,36 @@ class PlacesController < ApplicationController
             end
           end
         end
+
+        #Google Autocomplete
+        gpa = GooglePlacesAutocomplete.new
+        raw_places = gpa.suggest(lat, lng, query)
+
+        raw_places.each do |raw_place|
+          # Need to remove "political" and "route" types to stay in sync with mobile client
+          if !raw_place.types.include?("route")
+
+            place = Place.find_by_google_id(raw_place.id)
+
+            if place.nil?
+              #not here, and we need to fetch it
+              gp = GooglePlaces.new
+              place = Place.new_from_google_place(gp.get_place(raw_place.reference))
+              place.user = current_user unless current_user.nil?
+              place.client_application = current_client_application unless current_client_application.nil?
+              place.save!
+            end
+
+            @places.push(place)
+          end
+        end
+
       end
     end
-    
+
     respond_to do |format|
       format.html
-      format.json { render :json => {:places => @places.as_json( {:current_user => current_user, :user_view => true} ) }, :callback => params[:callback]  }
+      format.json { render :json => {:places => @places.as_json({:current_user => current_user, :user_view => true})}, :callback => params[:callback] }
     end
   end
 
@@ -139,28 +163,28 @@ class PlacesController < ApplicationController
     @place = Place.new
     @place.venue_types = [""]
     @place.name = params[:name] unless params[:name].nil?
-    
+
     if !params[:lat].nil?
       @lat = params[:lat].to_f
     else
       @lat = 0.0
     end
-    
+
     if !params[:lng].nil?
       @lng = params[:lng]
     else
       @lng = 0.0
     end
-    
+
     file = File.open(Rails.root.join("config/google_place_mapping.json"), 'r')
     content = file.read()
     @categories = JSON(content)
     @categories.each_pair do |key, val|
-      val.each_pair do |k,v|
+      val.each_pair do |k, v|
         val[k] = k #otherwise mapping to google
       end
     end
-    
+
     if request.referer.nil?
       @return_url = "/"
     else
@@ -170,7 +194,7 @@ class PlacesController < ApplicationController
         @return_url = request.referer
       end
     end
-    
+
     respond_to do |format|
       format.html
     end
@@ -178,31 +202,31 @@ class PlacesController < ApplicationController
 
   def confirm
     unless params['place']['address_components'].nil?
-      params['place']['address_components'] =JSON.parse( params['place']['address_components'] )
-      params['place']['address_components'] = params['place']['address_components'].each{|item| Hashie::Mash.new(item)}
-      
+      params['place']['address_components'] =JSON.parse(params['place']['address_components'])
+      params['place']['address_components'] = params['place']['address_components'].each { |item| Hashie::Mash.new(item) }
+
       address_array = []
       for component in params['place']['address_components']
-         address_array <<  Hashie::Mash.new( component )
+        address_array << Hashie::Mash.new(component)
       end
-      
-      address_dict = GooglePlaces.getAddressDict( address_array )
-      
+
+      address_dict = GooglePlaces.getAddressDict(address_array)
+
       if address_dict['number'] and address_dict['street']
         params['place']['street_address'] = address_dict['number'] + " " + address_dict['street']
       elsif address_dict['street']
-         params['place']['street_address'] = address_dict['street']
+        params['place']['street_address'] = address_dict['street']
       end
-      
+
       if address_dict['city'] and address_dict['province']
         params['place']['city_data'] = address_dict['city'] + ", " + address_dict['province']
       end
-      
-      params['place'].delete( 'address_components' )
+
+      params['place'].delete('address_components')
     end
-    
-    @place = Place.new( params[:place] )
-    
+
+    @place = Place.new(params[:place])
+
     if @place.valid?
       render :action => :confirm
     else
@@ -210,12 +234,12 @@ class PlacesController < ApplicationController
         @lat = @place.location[0]
         @lng = @place.location[1]
       end
-      
+
       file = File.open(Rails.root.join("config/google_place_mapping.json"), 'r')
       content = file.read()
       @categories = JSON(content)
       @categories.each_pair do |key, val|
-        val.each_pair do |k,v|
+        val.each_pair do |k, v|
           val[k] = k #otherwise mapping to google
         end
       end
@@ -237,8 +261,8 @@ class PlacesController < ApplicationController
     query = params[:query]
     category = params[:category]
 
-    if ( params[:query_type] )
-      query_type =  params[:query_type].downcase
+    if (params[:query_type])
+      query_type = params[:query_type].downcase
     else
       query_type = "popular"
     end
@@ -254,14 +278,14 @@ class PlacesController < ApplicationController
     #preprocess for query
     if query_type == "following" && current_user
       following_ids = current_user[:following_ids] << current_user.id
-      @perspectives = Perspective.query_near( loc, span, query, category ).
-        and(:uid.in => following_ids).includes(:user, :place).limit(n).entries
+      @perspectives = Perspective.query_near(loc, span, query, category).
+          and(:uid.in => following_ids).includes(:user, :place).limit(n).entries
     elsif query_type == "me" && current_user
-      search_ids = [ current_user.id ]
-      @perspectives = Perspective.query_near( loc, span, query, category ).
-        and(:uid.in => search_ids).includes(:user, :place).limit(n).entries
+      search_ids = [current_user.id]
+      @perspectives = Perspective.query_near(loc, span, query, category).
+          and(:uid.in => search_ids).includes(:user, :place).limit(n).entries
     else
-      @perspectives = Perspective.query_near( loc, span, query, category ).includes(:user, :place).limit(n).entries
+      @perspectives = Perspective.query_near(loc, span, query, category).includes(:user, :place).limit(n).entries
     end
 
     @places_dict = {}
@@ -272,18 +296,18 @@ class PlacesController < ApplicationController
       if current_user && perspective.user.id == current_user.id
         username = "You"
       else
-        username =  perspective.user.username
+        username = perspective.user.username
       end
       if @places_dict.has_key?(place.id)
         place = @places_dict[place.id]
         if username == "You"
-          place.users_bookmarking.insert(0, username )
+          place.users_bookmarking.insert(0, username)
         else
           place.users_bookmarking << username
         end
         place.placemarks << perspective
       else
-        place.users_bookmarking =  [username]
+        place.users_bookmarking = [username]
         @places_dict[place.id] = place
         place.placemarks = [perspective]
       end
@@ -300,12 +324,12 @@ class PlacesController < ApplicationController
 
     @places.each do |place|
       #add distance to in meters
-      place.distance = (1000 * Geocoder::Calculations.distance_between([lat,lng], [place.location[0],place.location[1]], :units =>:km)).floor
+      place.distance = (1000 * Geocoder::Calculations.distance_between([lat, lng], [place.location[0], place.location[1]], :units => :km)).floor
     end
 
     @places = @places.sort_by { |place| place.distance }
 
-    if query_type ==  "popular" and @places.count < 5
+    if query_type == "popular" and @places.count < 5
       gp = GooglePlaces.new
       #covers "barrie problem" of no content
       if category != nil and category.strip != ""
@@ -318,7 +342,7 @@ class PlacesController < ApplicationController
       for gplace in @google_places
         for place in @places
           if place.id == gplace.id
-            @google_places.delete( gplace )
+            @google_places.delete(gplace)
             break
           end
         end
@@ -328,7 +352,7 @@ class PlacesController < ApplicationController
       #this doesn't really work
       for gplace in @google_places
         #add distance to in meters
-        place = Place.new_from_google_place( gplace )
+        place = Place.new_from_google_place(gplace)
         @processed_google_places << place
       end
 
@@ -338,8 +362,8 @@ class PlacesController < ApplicationController
 
     logger.info "action: #{(Time.now - t) *1000}ms"
     respond_to do |format|
-        format.html
-        format.json { render :json => {:suggested_places => @places.as_json({:current_user =>current_user}) } }#, :ad => Advertisement.new( "Admob" ) } }
+      format.html
+      format.json { render :json => {:suggested_places => @places.as_json({:current_user => current_user})} } #, :ad => Advertisement.new( "Admob" ) } }
     end
 
   end
@@ -358,7 +382,7 @@ class PlacesController < ApplicationController
       @place.client_application = current_client_application unless current_client_application.nil?
     end
 
-    if ( params[:api_call] || verify_recaptcha(:model => @place, :message => "Invalid CAPTCHA")) && @place.save
+    if (params[:api_call] || verify_recaptcha(:model => @place, :message => "Invalid CAPTCHA")) && @place.save
       #by default, we placemark the new place
       track! :placemark
       @perspective= @place.perspectives.build()
@@ -367,7 +391,7 @@ class PlacesController < ApplicationController
 
       respond_to do |format|
         format.html { redirect_to :action => "show", :id => @place.id }
-        format.json { render :json => {:place => @place.as_json({:current_user => current_user}), :status =>"OK" } }
+        format.json { render :json => {:place => @place.as_json({:current_user => current_user}), :status => "OK"} }
       end
     else
       respond_to do |format|
@@ -379,8 +403,8 @@ class PlacesController < ApplicationController
 
 
   def highlight
-      #it's a direct request for a place in our db
-    @place = Place.forgiving_find( params[:id] )
+    #it's a direct request for a place in our db
+    @place = Place.forgiving_find(params[:id])
     current_user.highlighted_places << @place.id
     current_user.save
 
@@ -392,9 +416,9 @@ class PlacesController < ApplicationController
 
   def unhighlight
 
-    @place = Place.forgiving_find( params[:id])
+    @place = Place.forgiving_find(params[:id])
 
-    current_user.highlighted_places.delete( @place.id )
+    current_user.highlighted_places.delete(@place.id)
     current_user.save
 
     respond_to do |format|
@@ -403,14 +427,13 @@ class PlacesController < ApplicationController
   end
 
 
-
   def show
-    @place = Place.forgiving_find( params[:id])
+    @place = Place.forgiving_find(params[:id])
 
     if @place.nil? && params['google_ref'] # && current_user
-      #not here, and we need to fetch it
+                                           #not here, and we need to fetch it
       gp = GooglePlaces.new
-      @place = Place.new_from_google_place( gp.get_place( params['google_ref'] ) )
+      @place = Place.new_from_google_place(gp.get_place(params['google_ref']))
       @place.user = current_user
       @place.client_application = current_client_application unless current_client_application.nil?
       @place.save!
@@ -420,31 +443,29 @@ class PlacesController < ApplicationController
     raise ActionController::RoutingError.new('Not Found') unless !@place.nil?
 
     @follow_perspectives_count = 0
-    
+
     if current_user
       @my_perspective = current_user.perspective_for_place(@place)
-      @following_perspectives = current_user.following_perspectives_for_place( @place )
+      @following_perspectives = current_user.following_perspectives_for_place(@place)
       @following_perspectives_empty = []
       @follow_perspectives_count = @following_perspectives.count
-      
+
       for perspective in @following_perspectives
         if perspective.empty_perspective?
           @following_perspectives_empty << perspective
           @following_perspectives.delete(perspective)
         end
       end
-    end if
-
-    @all_perspectives = @place.perspectives.entries
+    end if @all_perspectives = @place.perspectives.entries
     @all_perspectives_empty = []
     if @all_perspectives
       @all_perspectives_count = @all_perspectives.count
     else
       @all_perspectives_count = 0
     end
-    
+
     perspectives_to_delete = []
-    
+
     for perspective in @all_perspectives
       if perspective.empty_perspective? && perspective != @my_perspective
         if @following_perspectives_empty
@@ -459,31 +480,31 @@ class PlacesController < ApplicationController
         perspectives_to_delete << perspective
       end
     end
-    
+
     for perspective in perspectives_to_delete
       @all_perspectives.delete(perspective)
     end
-    
+
     if @my_perspective and @all_perspectives.include?(@my_perspective)
       @all_perspectives.delete(@my_perspective)
     end
-    
+
     @else_perspective_count = @all_perspectives.count + @all_perspectives_empty.count
-    
+
     if params['rf']
-      @referring_user = User.find_by_username( params['rf'] )
+      @referring_user = User.find_by_username(params['rf'])
     else
       @referring_user = nil
     end
 
     respond_to do |format|
       format.html
-      format.json { render :json => @place.as_json({:detail_view => true, :current_user => current_user, :referring_user =>@referring_user}), :callback => params[:callback]  }
+      format.json { render :json => @place.as_json({:detail_view => true, :current_user => current_user, :referring_user => @referring_user}), :callback => params[:callback] }
     end
   end
 
   def edit
-    @place = Place.forgiving_find( params[:id] )
+    @place = Place.forgiving_find(params[:id])
   end
 
   def update
@@ -503,7 +524,7 @@ class PlacesController < ApplicationController
 
 
   protected
-  def realign_place_params( params )
+  def realign_place_params(params)
     place = {}
     place[:name] = params.delete(:name)
     place[:street_address] = params.delete(:street_address)
