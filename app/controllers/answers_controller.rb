b3e3431c9bf5d1d9d905d96c6765d6e9b4eb1f45
imackinn <imackinn@alumni.uwaterloo.ca>
2012-06-30 20:47:49 -0700

    fix upvoting, add vote and submit specs

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -38,7 +38,6 @@ class AnswersController < ApplicationController
     add_vote_to_history(@answer)
     @question.score = score +1
     @mixpanel.track_event("answer_submit", {:qid => @question.id})
-    track! :question_answer
     finished('question_answered', :reset => false)
 
     respond_to do |format|
@@ -64,7 +63,6 @@ class AnswersController < ApplicationController
 
     add_vote_to_history(@answer)
     @question.score += 1
-    track! :question_answer
     finished('question_answered', :reset => false)
 
     @mixpanel.track_event("upvote", {:qid => @question.id})
