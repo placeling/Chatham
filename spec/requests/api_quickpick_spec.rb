2abd00e76a31dc04733fb7f9647ea16f4ed134fb
imackinn <imackinn@alumni.uwaterloo.ca>
2012-07-10 15:36:50 -0400

    get rid of some pp outputs to cut down on console output

--- spec/requests/api_quickpick_spec.rb
+++ spec/requests/api_quickpick_spec.rb
@@ -8,30 +8,29 @@ describe "Quick Picks" do
     user3 = Factory.create(:user)
     user4 = Factory.create(:user) #control user
 
-    user.follow( user2 )
-    user.follow( user3 )
+    user.follow(user2)
+    user.follow(user3)
 
     place = Factory.create(:lib_square)
     place2 = Factory.create(:new_place)
     place3 = Factory.create(:place)
 
-    perspective = Factory.create(:perspective, :memo=>"one", :place =>place, :user =>user2)
-    perspective2 = Factory.create(:perspective, :memo=>"two", :place =>place, :user =>user3)
-    perspective3 = Factory.create(:perspective, :memo=>"three", :place =>place2, :user =>user3)
+    perspective = Factory.create(:perspective, :memo => "one", :place => place, :user => user2)
+    perspective2 = Factory.create(:perspective, :memo => "two", :place => place, :user => user3)
+    perspective3 = Factory.create(:perspective, :memo => "three", :place => place2, :user => user3)
 
     post_via_redirect user_session_path, 'user[login]' => user.username, 'user[password]' => user.password
 
     get suggested_places_path, {
-      :format => 'json',
-      :lat => '49.282049',
-      :lng => '-123.107772',
-      :query_type => 'following'
+        :format => 'json',
+        :lat => '49.282049',
+        :lng => '-123.107772',
+        :query_type => 'following'
     }
 
     response.status.should be(200)
 
-    showPlace = JSON.parse( response.body )
-    pp showPlace
+    showPlace = JSON.parse(response.body)
     showPlace['suggested_places'].count.should == 2
 
   end
@@ -42,8 +41,8 @@ describe "Quick Picks" do
     user3 = Factory.create(:user)
     user4 = Factory.create(:user) #control user
 
-    user.follow( user2 )
-    user.follow( user3 )
+    user.follow(user2)
+    user.follow(user3)
 
     place = Factory.create(:lib_square)
     place2 = Factory.create(:new_place)
@@ -52,24 +51,24 @@ describe "Quick Picks" do
     place4 = Factory.create(:lib_square, :name => "ternary place")
     place5 = Factory.create(:lib_square, :name => "quadranary place")
 
-    perspective = Factory.create(:perspective, :memo=>"one", :place =>place, :user =>user2)
-    perspective2 = Factory.create(:perspective, :memo=>"two", :place =>place2, :user =>user3)
-    perspective3 = Factory.create(:perspective, :memo=>"three", :place =>place3, :user =>user4)
-    perspective4 = Factory.create(:perspective, :memo=>"three", :place =>place4, :user =>user2)
-    perspective5 = Factory.create(:perspective, :memo=>"three", :place =>place5, :user =>user3)
+    perspective = Factory.create(:perspective, :memo => "one", :place => place, :user => user2)
+    perspective2 = Factory.create(:perspective, :memo => "two", :place => place2, :user => user3)
+    perspective3 = Factory.create(:perspective, :memo => "three", :place => place3, :user => user4)
+    perspective4 = Factory.create(:perspective, :memo => "three", :place => place4, :user => user2)
+    perspective5 = Factory.create(:perspective, :memo => "three", :place => place5, :user => user3)
 
     post_via_redirect user_session_path, 'user[login]' => user.username, 'user[password]' => user.password
 
     get suggested_places_path, {
-      :format => 'json',
-      :lat => '49.282049',
-      :lng => '-123.107772',
-      :query_type =>'popular'
+        :format => 'json',
+        :lat => '49.282049',
+        :lng => '-123.107772',
+        :query_type => 'popular'
     }
 
     response.status.should be(200)
 
-    showPlace = JSON.parse( response.body )
+    showPlace = JSON.parse(response.body)
     showPlace['suggested_places'].count.should == 5
 
   end
