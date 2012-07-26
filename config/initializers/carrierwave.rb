242a7699e209eff47e2aa9b5831a874a81fa16b2
imackinn <imackinn@alumni.uwaterloo.ca>
2012-07-26 14:13:09 -0700

    don't use persistent connections in hopes of fixing the exconn timeouts

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,18 +1,21 @@
- require 'carrierwave/mongoid'
+require 'carrierwave/mongoid'
 
 CarrierWave.configure do |config|
-    config.fog_public     = true         # optional, defaults to true
-    config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
-    config.fog_credentials = {
-        :provider               => 'AWS',       # required
-        :aws_access_key_id      => 'AKIAJ5CV27YWN6ERL32Q',       # required
-        :aws_secret_access_key  => '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'       # required
-    }
+  config.fog_public = true # optional, defaults to true
+  config.fog_attributes = {'Cache-Control' => 'max-age=315576000'} # optional, defaults to {}
+  config.fog_credentials = {
+      :provider => 'AWS', # required
+      :aws_access_key_id => 'AKIAJ5CV27YWN6ERL32Q', # required
+      :aws_secret_access_key => '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', # required
+      :persistent => false # This is required to prevent write timeouts from PUT requests to S
+  }
 
-   if Rails.env.production?
-      config.fog_directory  = 'chatham-production'                     # required
-   else
-      config.fog_directory  =  'chatham-test'                    # required
-   end
+  if Rails.env.production?
+    config.fog_directory = 'chatham-production' # required
+  else
+    config.fog_directory = 'chatham-test' # required
+  end
 
 end
+
+#Excon.defaults[:write_timeout] = 500 #try this if the timeouts keep happening
