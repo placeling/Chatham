44babbbefd97b4a6018c5334933173e18077e44c
imackinn <imackinn@alumni.uwaterloo.ca>
2012-07-27 14:42:52 -0700

    only send weekly email if we have a non-zero location

--- app/workers/weekly_email.rb
+++ app/workers/weekly_email.rb
@@ -5,16 +5,18 @@ class WeeklyEmail
 
     if Rails.env.production?
       User.all.each do |user|
-        if user.weekly_email? && !user.loc.nil?
+        if user.weekly_email? && !user.loc.nil? && user.loc != [0.0, 0.0]
           mail = Notifier.weekly(user.id)
           mail.deliver unless mail.to == nil
         end
       end
     else
       User.all.limit(20).each do |user|
-        if user.weekly_email? && !user.loc.nil?
+        if user.weekly_email? && !user.loc.nil? && user.loc != [0.0, 0.0]
           mail = Notifier.weekly(user.id)
           #mail.deliver unless mail.to == nil don't actually deliver by default
+        elsif user.weekly_email?
+          puts "skip #{user.username} because loc= #{user.loc}"
         end
       end
     end
