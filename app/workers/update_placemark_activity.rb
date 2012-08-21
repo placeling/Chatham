80fe3a388d40879bb223698a5071fcbd1fa0808a
imackinn <imackinn@alumni.uwaterloo.ca>
2012-08-21 16:07:19 -0700

    refactor the facebook posts to a different queue job, handle invalid credentials as an expired auth

--- app/workers/update_placemark_activity.rb
+++ app/workers/update_placemark_activity.rb
@@ -26,17 +26,8 @@ class UpdatePlacemarkActivity
     activity.push_to_followers(actor1)
 
     if fb_post && actor1.post_facebook? && Rails.env.production?
-      image_url=nil
-      for picture in perspective.pictures
-        if !picture.fb_posted
-          image_url = picture.main_url(nil)
-          picture.fb_posted = true
-          picture.save
-          break
-        end
-      end
-
-      actor1.facebook.put_connections("me", "placeling:set", :placemark => perspective.og_path)
+      Resque.enqueue(FacebookPost, actor1.id, "placeling:set", {:placemark => perspective.og_path})
+      #actor1.facebook.put_connections("me", "placeling:set", :placemark => perspective.og_path)
     end
 
     if twitter_post && actor1.twitter && Rails.env.production?
