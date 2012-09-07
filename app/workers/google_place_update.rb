b7ad13f332f42b59d36c9756fce92eddecf90c17
imackinn <imackinn@alumni.uwaterloo.ca>
2012-09-07 09:25:41 -0700

    CHA-17, save the place if we dont' get anything back, bump up to 300, reduce logging to exception cases

--- app/workers/google_place_update.rb
+++ app/workers/google_place_update.rb
@@ -4,7 +4,7 @@ class GooglePlaceUpdate
   def self.perform()
 
     if Rails.env.production?
-      places = Place.where(:updated_at.lt => 1.month.ago).limit(100)
+      places = Place.where(:updated_at.lt => 1.month.ago).limit(300)
     else
       places = Place.where(:updated_at.lt => 1.month.ago).limit(10)
     end
@@ -14,7 +14,7 @@ class GooglePlaceUpdate
     places.each do |place|
 
       if place.perspective_count == 0 && Question.any_of({'answers.place_id' => place.id}).count == 0
-        RESQUE_LOGGER.info "#{place.name} - #{place.id}, no perspectives or answers, so delete"
+        #RESQUE_LOGGER.info "#{place.name} - #{place.id}, no perspectives or answers, so delete"
         place.destroy
 
       elsif place.google_ref
@@ -31,7 +31,7 @@ class GooglePlaceUpdate
               #weird case where changing to already existing id
               RESQUE_LOGGER.info "updating #{place.name}, existing google ID, need to merge #{oldPlace.name} and #{merge_place.name} to #{updatedPlace.name}"
             else
-              RESQUE_LOGGER.info "updating #{place.name}, updated google ID, same name"
+              #RESQUE_LOGGER.info "updating #{place.name}, updated google ID, same name"
               place = place.update_from_google_place(updatedPlace)
               place.save!
             end
@@ -46,6 +46,7 @@ class GooglePlaceUpdate
           end
         else
           RESQUE_LOGGER.info "didn't get place back for #{place.name} - #{place.id}"
+          place.save #not sure what to do with these yet, so just renew their lease on life
         end
         sleep(2)
       end
