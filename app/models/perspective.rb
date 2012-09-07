5733d648db772e15a9e30b5c07c7b290c6340efb
imackinn <imackinn@alumni.uwaterloo.ca>
2012-09-07 13:08:36 -0700

    dont' add update_flag to the thing

--- app/models/perspective.rb
+++ app/models/perspective.rb
@@ -211,7 +211,7 @@ class Perspective
   def get_place_data
     self.place_location = self.place.location
     self.place_stub = PlaceStub.new
-    place_attributes = self.place.attributes.except('address_components', 'cid', 'user_id', 'slug', 'html_attributions')
+    place_attributes = self.place.attributes.except('address_components', 'cid', 'user_id', 'slug', 'html_attributions', "update_flag")
     self.place_stub.attributes = place_attributes
   end
 
