6e5e7edde6ddb04d56af24c73fecc293a613ecba
imackinn <imackinn@alumni.uwaterloo.ca>
2012-09-17 14:23:13 -0700

    add a logger for grabbing urls to better diagnose errors

--- app/workers/get_perspective_picture.rb
+++ app/workers/get_perspective_picture.rb
@@ -1,34 +1,36 @@
 class GetPerspectivePicture
-    @queue = :perspective_queue
-    def self.perform(perspective_id, photo_urls)
-        @perspective = Perspective.find( perspective_id )
+  @queue = :perspective_queue
 
-        for photo_url in photo_urls
-          found = false
-          for picture in @perspective.pictures
-            if picture.remote_url && picture.remote_url == photo_url
-              found = true
-              break
-            end
-          end
+  def self.perform(perspective_id, photo_urls)
+    @perspective = Perspective.find(perspective_id)
 
-          if !found
-            picture = @perspective.pictures.build()
-            begin
-              uri = URI( photo_url )
-
-              if uri.host == "www.urbanspoon.com"
-                break  #special case, these give 403 errors
-              end
-
-              picture.remote_url = photo_url # for us, to keep track
-              picture.remote_image_url = photo_url
-              picture.save
-            rescue => ex
-              Airbrake.notify( ex , {:error_message =>"Problem loading url #{photo_url}"} )
-              @perspective.pictures.delete(picture)
-            end
-          end
+    RESQUE_LOGGER.info "#{Time.now.strftime('%Y-%m-%d %H:%M:%S')} - Grabbing #{@perspective.user.username}'s photos for perspectives: #{@perspective.id}\n#{photo_urls.join("\n")}"
+    for photo_url in photo_urls
+      found = false
+      for picture in @perspective.pictures
+        if picture.remote_url && picture.remote_url == photo_url
+          found = true
+          break
         end
+      end
+
+      if !found
+        picture = @perspective.pictures.build()
+        begin
+          uri = URI(photo_url)
+
+          if uri.host == "www.urbanspoon.com"
+            break #special case, these give 403 errors
+          end
+
+          picture.remote_url = photo_url # for us, to keep track
+          picture.remote_image_url = photo_url
+          picture.save
+        rescue => ex
+          Airbrake.notify(ex, {:error_message => "Problem loading url #{photo_url}"})
+          @perspective.pictures.delete(picture)
+        end
+      end
     end
+  end
 end
