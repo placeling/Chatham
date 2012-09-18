fd9fa1d990fd3c4b153d00d7755b31606c33f9b5
imackinn <imackinn@alumni.uwaterloo.ca>
2012-09-17 17:23:33 -0700

    fix phantomjs call

--- app/workers/render_tour.rb
+++ app/workers/render_tour.rb
@@ -1,18 +1,18 @@
 class RenderTour
   @queue = :tour
-  
+
   def self.perform(tour_id, target_url)
     @tour = Tour.find(tour_id)
-    
-    outdir = Rails.root.join('public','uploads',@tour.id.to_s+'.png')
-    rasterize = Rails.root.join('lib','rasterize.js')
-    
-    RESQUE_LOGGER.info "#{target_url} #{outdir}"
-    output = `phantomjs #{rasterize} #{target_url} #{outdir}`
+
+    outdir = Rails.root.join('public', 'uploads', @tour.id.to_s+'.png')
+    rasterize = Rails.root.join('lib', 'rasterize.js')
+
+    RESQUE_LOGGER.info "#{Time.now.strftime('%Y-%m-%d %H:%M:%S')} - Tour Creation: #{target_url} #{outdir}"
+    output = `phantomjs --local-to-remote-url-access=yes --ignore-ssl-errors=yes #{rasterize} #{target_url} #{outdir}`
     RESQUE_LOGGER.info "finished phantomjs"
-    
+
     @tour.infographic = File.open(outdir)
-    
+
     @tour.rendered = true
     @tour.save!
   end
