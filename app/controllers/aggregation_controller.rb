d4840edfd6e1fdabcab2fb52c93a8f13fe85e46e
imackinn <imackinn@alumni.uwaterloo.ca>
2012-10-04 13:37:05 -0700

    add some aggregation stuff for users.

--- app/controllers/aggregation_controller.rb
+++ app/controllers/aggregation_controller.rb
@@ -2,6 +2,12 @@ class AggregationController < ApplicationController
 
   def index
 
+    if params[:lat].nil? || params[:lng].nil?
+      @users = User.top_nearby(@default_lat, @default_lng, 6)
+    else
+      @users = User.top_nearby(params[:lat], params[:lng], 6)
+    end
+
 
     respond_to do |format|
       format.html { render :layout => 'bootstrap' }
