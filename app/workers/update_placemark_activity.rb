8e20c8a750563488a0ecbd4a5901f73f72cba84f
imackinn <imackinn@alumni.uwaterloo.ca>
2012-10-09 18:02:24 -0700

    be more sure about the stuff

--- app/workers/update_placemark_activity.rb
+++ app/workers/update_placemark_activity.rb
@@ -17,7 +17,7 @@ class UpdatePlacemarkActivity
 
     #check if a "recent" activity, most recent 20
     actor1.activity_feed.activities.each do |act|
-      if act.activity_type == "UPDATE_PERSPECTIVE" || act.activity_type == "NEW_PERSPECTIVE" && activity.subject == act.subject
+      if (act.activity_type == "UPDATE_PERSPECTIVE" || act.activity_type == "NEW_PERSPECTIVE") && activity.subject == act.subject
         return
       end
     end
