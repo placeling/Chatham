79785a2bd0a28ce9dd19926e67fe945fc993a14e
Lindsay Watt <lindsayrgwatt@gmail.com>
2012-10-11 12:22:56 -0700

    Migration to add UserTour subdocs to missed users

---
+++ db/migrate/20121011192130_attach_user_tour_subdocs_again.rb
@@ -0,0 +1,16 @@
+class AttachUserTourSubdocsAgain < Mongoid::Migration
+  def self.up
+    users = User.all()
+    
+    users.each do |user|
+      if !user.user_tour
+        user.create_user_tour
+      end
+      
+      user.save
+    end
+  end
+
+  def self.down
+  end
+end
