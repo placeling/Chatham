0d37b7abb909cbec01995c3288b3f3e96d1d89fa
imackinn <imackinn@alumni.uwaterloo.ca>
2012-10-26 12:44:04 -0700

    use CDN in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,6 +33,7 @@ Chatham::Application.configure do
 
   config.assets.compress = true
   config.assets.digest = true
+  config.action_controller.asset_host = "https://d19v2hl3110mg7.cloudfront.net"
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "dmznjo0da80tm.cloudfront.net"
