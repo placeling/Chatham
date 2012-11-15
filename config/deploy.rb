309d302fb8f50d1a968dd9d8a39e890c42753d70
imackinn <imackinn@alumni.uwaterloo.ca>
2012-11-15 10:39:35 -0800

    update github to placeling account

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,7 +32,7 @@ task :staging do
 end
 
 default_run_options[:pty] = true # Must be set for the password prompt from git to work
-set :repository, "git@github.com:imackinn/Chatham.git" # Your clone URL
+set :repository, "git@github.com:placeling/Chatham.git" # Your clone URL
 set :scm, "git"
 
 set :deploy_to, "/var/www/apps/#{application}"
