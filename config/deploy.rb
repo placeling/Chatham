d39bc4d6e9d4fbab6bc6e1c8840f3ed860fca469
imackinn <imackinn@alumni.uwaterloo.ca>
2012-11-26 20:38:19 -0800

    update based on experience from blackwood

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,9 +5,10 @@ set :application, "chatham"
 set :rvm_ruby_string, "ruby-1.9.3-p125"
 require "rvm/capistrano" # Load RVM's capistrano plugin.
 
-before 'deploy:setup', 'ubuntu:required_packages'
 before 'deploy:setup', 'rvm:install_rvm'
 before 'deploy:setup', 'rvm:install_ruby'
+before 'deploy:setup', "ubuntu:required_packages"
+before 'deploy:setup', 'ubuntu:service_gems'
 
 after "deploy:create_symlink", "deploy:restart_workers"
 after "deploy:create_symlink", "deploy:restart_scheduler"
@@ -78,13 +79,13 @@ end
 namespace :ubuntu do
   task :required_packages, :roles => :app do
     run 'sudo apt-get update'
-    run 'sudo apt-get install git-core ruby  ruby-dev rubygems libxslt-dev libxml2-dev libcurl4-openssl-dev imagemagick'
+    run 'sudo apt-get install git-core ruby  ruby-dev rubygems libxslt-dev libxml2-dev libcurl4-openssl-dev imagemagick nodejs'
     run 'sudo apt-get install zlib1g-dev libssl-dev libyaml-dev libsqlite3-0  libsqlite3-dev sqlite3 libxml2-dev libxslt-dev  autoconf libc6-dev ncurses-dev'
     run 'sudo apt-get install build-essential bison openssl libreadline6 libreadline6-dev curl libtool libpcre3 libpcre3-dev'
   end
 
   task :service_gems, :roles => :app do
-    run 'gem install bundler passenger scout request-log-analyzer'
+    run 'sudo gem install bundler passenger scout request-log-analyzer'
   end
 
 end
