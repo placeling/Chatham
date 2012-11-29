38a72fe67450f539ea8879b37ca04e33436c8430
imackinn <imackinn@alumni.uwaterloo.ca>
2012-11-29 13:20:21 -0800

    grumble grumble environemnt of enviornment

--- config/initializers/redis_resque.rb
+++ config/initializers/redis_resque.rb
@@ -4,7 +4,7 @@ require 'resque/failure/redis'
 
 # REDIS_CONFIG = YAML.load(File.open(Rails.root.join("config/redis.yml"))) opened in application.rb
 
-redis_base = Redis.new(REDIS_CONFIG[::Rails.env].symbolize_keys!)
+redis_base = Redis.new(REDIS_CONFIG.symbolize_keys!)
 
 Resque.redis = redis_base
 Resque.redis.namespace = "resque:placeling"
@@ -24,7 +24,7 @@ end
 
 Resque.schedule = YAML.load_file(File.join(Rails.root, 'config/resque_schedule.yml'))
 
-$redis = Redis::Namespace.new(REDIS_CONFIG[::Rails.env][:namespace], :redis => redis_base)
+$redis = Redis::Namespace.new(REDIS_CONFIG[:namespace], :redis => redis_base)
 $redis.flushdb if Rails.env.test?
 
 
