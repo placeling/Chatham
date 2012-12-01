55ffea92fd1b8bfe7999aac61f132249f7329aed
imackinn <imackinn@alumni.uwaterloo.ca>
2012-11-30 16:23:44 -0800

    setup service for sending invalidation commands to the publisher pub/sub

--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -11,8 +11,13 @@ class Publisher
   belongs_to :user
 
   validates_presence_of :user
+  after_save :invalidate_cache
 
   def category_for(category)
     return self.publisher_categories.where(:slug => category).first
+  end
+
+  def invalidate_cache
+    $redis.publish "invalidations", {'model' => "publisher", "id" => self.id}.to_json
   end
 end
