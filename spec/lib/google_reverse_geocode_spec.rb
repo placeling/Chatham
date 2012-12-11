c7c72a439feac4fce86e2f39b5e0867765249b48
imackinn <imackinn@alumni.uwaterloo.ca>
2012-12-11 15:22:23 -0800

    fix test results slightly

--- spec/lib/google_reverse_geocode_spec.rb
+++ spec/lib/google_reverse_geocode_spec.rb
@@ -7,21 +7,21 @@ describe GoogleReverseGeocode do
   end
 
   it "should find a result here" do
-    address = @grg.reverse_geocode(49.28345,-123.10998) # 320 W Cordova St
-    
+    address = @grg.reverse_geocode(49.28345, -123.10998) # 320 W Cordova St
+
     street_number = nil
     address.address_components.each do |component|
       if component["types"].include? "street_number"
         street_number = component["long_name"]
       end
     end
-    
-    street_number.should == "320"
+
+    street_number.should == "309"
   end
 
   it "should not find a result here" do
-    address = @grg.reverse_geocode(49.391,-123.095) # Backcountry near Grouse Moutain
-    
+    address = @grg.reverse_geocode(49.391, -123.095) # Backcountry near Grouse Moutain
+
     address.should == nil
   end
 end
