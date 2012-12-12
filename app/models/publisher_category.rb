91aeaeb3b106439dfdda9f63ec0222b4bdba0ed7
imackinn <imackinn@alumni.uwaterloo.ca>
2012-12-11 16:18:28 -0800

    add url caching of images

--- app/models/publisher_category.rb
+++ app/models/publisher_category.rb
@@ -7,11 +7,37 @@ class PublisherCategory
   field :module_type, :type => Integer, :default => 0
   field :tags, :type => String
 
+  field :creation_environment, :type => String
+  field :main_cache_url, :type => String
+
+  before_save :cache_urls
+
+  def cache_urls
+    if self.creation_environment.nil?
+      self.creation_environment = Rails.env
+      self.main_cache_url = self.image_url(:main)
+    end
+  end
+
+  def main_url
+    if Rails.env == self.creation_environment
+      self.image_url(:main)
+    elsif main_cache_url
+      main_cache_url
+    else
+      self.image_url(:main)
+    end
+  end
+
   mount_uploader :image, CategoryUploader
 
   embedded_in :publisher
 
   slug :name, :permanent => true
 
+
+  def as_json(options={})
+    self.attributes.merge(:image_url => self.main_url)
+  end
 
 end
