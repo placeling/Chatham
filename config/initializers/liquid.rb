4855f779b5bc5572dba1a2787171995b3ca866ca
imackinn <imackinn@alumni.uwaterloo.ca>
2013-01-22 19:00:01 -0800

    rip out bootstrap from the home liquid render for now

--- config/initializers/liquid.rb
+++ config/initializers/liquid.rb
@@ -4,3 +4,4 @@ require 'liquid/category'
 
 Liquid::Template.register_tag('CategoryImage', CategoryImage)
 Liquid::Template.register_tag('category', Category)
+Liquid::Template.register_tag('Category', Category)
