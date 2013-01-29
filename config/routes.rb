77b5057b7f14015c3a9568460201d33c08357c2d
Lindsay Watt <lindsayrgwatt@gmail.com>
2013-01-28 20:07:43 -0800

    Created Placeling for Publishers

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Chatham::Application.routes.draw do
   get "/locals", :to => 'admin#local', :as => :local
   get "/tour", :to => 'admin#tour', :as => :tour
   get "/bloggers", :to => 'admin#bloggers', :as => :bloggers
+  get "/publisher", :to => 'admin#publisher', :as => :publisher
   get "/bloggers/plans", :to => 'admin#blogger_plans', :as => :blogger_plans
   get "/bloggers/matrix", :to => 'admin#blogger_matrix', :as => :blogger_matrix
   get "/admin/investors", :to => 'admin#investors', :as => :investors
