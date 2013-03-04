04e41f585170734fb41b65dbce7b4a4b7b1ef7a2
imackinn <imackinn@alumni.uwaterloo.ca>
2013-03-04 15:25:49 -0800

    have the "nearby" take a timestamp so we can check for the past day for twitter reasons

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -15,6 +15,12 @@ class RecommendationsController < ApplicationController
     lat = params[:lat].to_f
     lng = params[:lng].to_f
 
+    if params[:since]
+      since = params[:since].to_f
+    else
+      since = 1.week.ago
+    end
+
     geonear = BSON::OrderedHash.new()
     geonear["$near"] = [lat, lng]
     geonear["$maxDistance"] = 0.1
@@ -22,7 +28,7 @@ class RecommendationsController < ApplicationController
     @bloggers = Blogger.where("entries.location" => geonear).entries
     placed_bloggers = []
     @bloggers.each do |blogger|
-      blogger.entries.delete_if { |entry| entry.created_at < 1.week.ago || entry.place_id.nil? }
+      blogger.entries.delete_if { |entry| entry.created_at < since || entry.place_id.nil? }
       placed_bloggers << blogger unless blogger.entries.count == 0 || placed_bloggers.include?(blogger)
     end
 
