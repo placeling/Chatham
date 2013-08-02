306ce3aa78dd19ab63564ada9afd5dc4992ce6b1
imackinn <imackinn@alumni.uwaterloo.ca>
2013-08-02 10:13:55 -0700

    cleanup some path things

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,30 +5,8 @@ class ApplicationController < ActionController::Base
 
   # protect_from_forgery TODO: might want this back
 
-  helper_method :user_location
-  helper_method :return_to_link
-
   alias :logged_in? :user_signed_in?
 
-  def return_to_link
-    if session[:"user_return_to"]
-      session[:"user_return_to"]
-    else
-      if !current_user.nil?
-        user_path(current_user)
-      end
-    end
-  end
-
-  def after_sign_out_path_for(resource)
-    if session[:"user_return_to"]
-      return session[:"user_return_to"].to_s
-    elsif request.referer
-      return request.referer
-    else
-      return "/"
-    end
-  end
 
   def login_required
     authenticate_user!
