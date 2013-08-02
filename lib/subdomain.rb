9e524c87a602573f81a4b63368874180c8e82c53
imackinn <imackinn@alumni.uwaterloo.ca>
2013-08-02 13:04:06 -0700

    get rid of subdomain

--- lib/subdomain.rb
+++
@@ -1,7 +0,0 @@
-class Subdomain
-  @@mobile_enabled = ['gridto', 'lindsayrgwatt', "imack", "georgiastraight", "vanmag", "vanart"]
-
-  def self.matches?(request)
-    request.subdomain.present? && request.subdomain != 'www' && @@mobile_enabled.include?(request.subdomain)
-  end
-end
